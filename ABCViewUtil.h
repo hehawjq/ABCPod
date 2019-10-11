6a68aaf1d68b4ed839e621999b6558af37c55ffe
Blue <wujunqiu@100tal.com>
2019-10-11 17:38:21 +0800

    add framework

---
+++ ABCViewUtil.h
@@ -0,0 +1,19 @@
+//
+//  ABCViewUtil.h
+//  AIClass
+//
+//  Created by office on 2019/3/25.
+//  Copyright © 2019年 TAL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ABCViewUtil : NSObject
+
++ (CAShapeLayer *)setCorner:(CGRect)bounds rectCorner:(NSUInteger)rectCorner;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Frameworks/ArcSoftFaceSDK.framework/Headers/ArcSoftFaceSDK.h
@@ -0,0 +1,21 @@
+//
+//  ArcSoftFaceSDK.h
+//  ArcSoftFaceSDK
+//
+//  Created by zhangkai on 2018/11/22.
+//  Copyright © 2018年 zhangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ASFOpenGLView.h"
+#import "ASFRenderViewController.h"
+
+//! Project version number for ArcSoftFaceSDK.
+FOUNDATION_EXPORT double ArcSoftFaceSDKVersionNumber;
+
+//! Project version string for ArcSoftFaceSDK.
+FOUNDATION_EXPORT const unsigned char ArcSoftFaceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcSoftFaceSDK/PublicHeader.h>
+
+
